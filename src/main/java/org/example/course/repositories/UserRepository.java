162afe315f63c3232847087d41aea511f6bf935f
Gabriel Oliveira <gmdo0420@gmail.com>
2025-09-21 18:41:04 -0300

    :sparkles: feat: Adiciona UserService e rota Get UserId.

--- src/main/java/org/example/course/repositories/UserRepository.java
+++ src/main/java/org/example/course/repositories/UserRepository.java
@@ -3,5 +3,4 @@ package org.example.course.repositories;
 import org.example.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-}
+public interface UserRepository extends JpaRepository<User, Long> { }
